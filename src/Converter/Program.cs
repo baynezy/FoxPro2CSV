172333d8648a4e852428e4a242654b5d6a241303
Simon Baynes <baynezy@gmail.com>
2023-12-29 11:28:34 +0000

    Convert project to use Cocona
    Closes #12

--- src/Converter/Program.cs
+++ src/Converter/Program.cs
@@ -1,28 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-using CommandLine;
-using Converter;
+﻿using Cocona;
+using Converter.Commands.ConvertDbfToCsv;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 
-Parser.Default.ParseArguments<Options>(args)
-    .WithParsed(options =>
-    {
-        Console.WriteLine("Visual FoxPro 2 CSV Converter v1.0.0");
-        Console.WriteLine("=====================================");
-        Console.WriteLine();
-        
-        var builder = Host.CreateDefaultBuilder(args);
-        builder.ConfigureServices(services =>
-        {
-            services.AddSingleton(options);
-            services.AddSingleton<ConverterService>();
-        });
-        
-        var app = builder.Build();
-        
-        var converter = app.Services.GetRequiredService<ConverterService>();
-        converter.Convert();
-    });
+var builder = CoconaApp.CreateBuilder();
+builder.Services.AddSingleton<ConvertDbfToCsvCommand>();
 
+var app = builder.Build();
 
+Console.WriteLine("Visual FoxPro 2 CSV Converter v1.0.0");
+Console.WriteLine("=====================================");
+Console.WriteLine();
+
+app.AddCommands<ConvertDbfToCsvCommand>();
+
+app.Run();
